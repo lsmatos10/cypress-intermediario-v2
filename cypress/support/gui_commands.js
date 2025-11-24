408cbf61f9c5a8b68fdf8f840bfded55f3b82149
Leonardo Matos <devleomatos@gmail.com>
2025-11-23 21:25:28 -0300

    adicionar teste de logout e login no cache

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
 ) => {
   const login = () => {
     cy.visit('/users/sign_in')
@@ -10,5 +11,27 @@ Cypress.Commands.add('login', (
     cy.get("[data-qa-selector='sign_in_button']").click()
   }
 
-  login()
+  const options = {
+    cacheAcrossSpecs: true,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
+    login()
+  }
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.get('.qa-user-avatar').click()
+  cy.contains('Sign out').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
 })
