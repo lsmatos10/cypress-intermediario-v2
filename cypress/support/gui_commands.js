97f30bfb1921e9d044bb432c99fdf33c06b43fe2
Leonardo Matos <devleomatos@gmail.com>
2025-12-09 20:02:04 -0300

    criacao de issue

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -11,8 +11,15 @@ Cypress.Commands.add('login', (
     cy.get("[data-qa-selector='sign_in_button']").click()
   }
 
+  const validate = () => {
+    cy.visit('/')
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
+  }
+
   const options = {
     cacheAcrossSpecs: true,
+    validate,
   }
 
   if (cacheSession) {
@@ -34,4 +41,12 @@ Cypress.Commands.add('gui_createProject', project => {
     cy.get('#project_description').type(project.description)
     cy.get('.qa-initialize-with-readme-checkbox').check()
     cy.contains('Create project').click()
+})
+
+Cypress.Commands.add('gui_createIssue', issue => {
+  cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+  cy.get('.qa-issuable-form-title').type(issue.title)
+  cy.get('.qa-issuable-form-description').type(issue.description)
+  cy.contains('Submit issue').click()
 })
